ff11ba4c5eec5637f1abd6606f60bc65f9d70201
unknown <jokas3@gmail.com>
2025-01-12 20:11:03 +0100

    feat: Client React App basic setup complete
    Setup the initial react application structure with application context
    and routing

---
+++ Client/Application.jsx
@@ -0,0 +1,23 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./View/Home";
+
+export const ApplicationContext = createContext();
+
+const Application = () => {
+
+    const gateway = process.env.REACT_APP_GATEWAY_URL;
+
+    return(
+        <ApplicationContext.Provider value={ {
+            gateway
+        } }>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                </Routes>
+            </BrowserRouter>
+        </ApplicationContext.Provider>
+    )
+}
+
+export default Application;
---
+++ Client/View/Home.jsx
@@ -0,0 +1,8 @@
+const Home = () => {
+
+    return(
+        <div>HII</div>
+    )
+}
+
+export default Home;
